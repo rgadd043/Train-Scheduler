d4b3a4d4abbf7495bdf664da6194d56fe9901427
Ryan Gadd <ryangadd@Ryans-MacBook-Pro.local>
2017-03-31 18:02:06 -0500

    Train Schedule homework

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -13,7 +13,9 @@ firebase.initializeApp(config);
   var firstTrain;
   var frequency;
   var nextArrival;
-  var minutesAway;
+  var minutesAway; // Train
+  var hour;
+  var minutes; // Clock
 
 // Retrieve values from form
   $("#add-train").on("click", function() {
@@ -22,15 +24,57 @@ firebase.initializeApp(config);
       destination = $("#destination-input").val();
       firstTrain = $("#first-input").val();
       frequency = $("#frequency-input").val();
-    console.log($("#train-input").val());
-    console.log($("#destination-input").val());
-    console.log($("#first-input").val());
-    console.log($("#frequency-input").val());
-        //Push to Firebase
+
+// Push to Firebase
           firebase.database().ref().push({
             trainName: trainName,
             destination: destination,
             firstTrain: firstTrain,
             frequency: frequency,
           });
+
+// Clear HTML values
+  // trainName = $("#train-input").val("");
+  // destination = $("#destination-input").val("");
+  // firstTrain = $("#first-input").val("");
+  // frequency = $("#frequency-input").val("");
+
   });
+
+// Clock function
+  function setClock() {
+  $("#timeClock").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+  }
+// Display clock
+  setInterval(setClock, 1000);
+
+// Listen for children added & perform time conversion
+  firebase.database().ref().on("child_added", function(snapshot) {
+    trainName = snapshot.val().trainName;
+    destination = snapshot.val().destination;
+    firstTrain = snapshot.val().firstTrain;
+    frequency = snapshot.val().frequency;
+
+// Convert time (see comments below)
+  firstTrain = moment(firstTrain, 'hh:mm a');
+  nextArrival = firstTrain.format('hh:mm a');
+    while (moment(nextArrival, 'hh:mm a').isBefore(moment(), 'hh:mm a'))
+    {
+      nextArrival = moment(nextArrival, 'hh:mm a').add(frequency, "minutes").format('hh:mm a');
+    };
+     minutesAway = Math.ceil(moment.duration(moment(nextArrival, "HH:mm a").diff(moment(), "HH:mm a")).asMinutes());
+
+// Append submitted data into new table row
+    $(".scheduleData").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency +
+    "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
+});
+
+// Line 59: Pass firstTrain value into moment, and format to hour, minutes, and AM/PM
+// Line 60: Set nextArrival to the value of firstTrain and format. This is necessary since the value of firstTrain
+// Line 60: remains static due to value submitted.
+// Line 61: Run a while loop if the time of the first train (nextArrival) IS BEFORE the current time.
+// Line 63: WHILE the nextArrival is < current time, add the frequency value until nextArrival is > current time.
+// Line 65: Assign difference between nextArrival and current time to minutesAway.
+// Line 66: Using Math.ceil to round milliseconds up, and asMinutes() to format to minutes.
+// Line 68: Append submitted data to table.
